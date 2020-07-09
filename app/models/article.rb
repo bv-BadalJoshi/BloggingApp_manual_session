edb1dd63ed9f7987548aecdec86d3cc8a1cc0ea6
bv-BadalJoshi <Badal@blogvault.net>
2020-07-09 17:34:01 +0530

    Fix #1 Adding user functionality to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+	belongs_to :user
 	validates :title, presence: true, length: { minimum: 2, maximum: 50 }
 	validates :description, presence: true, length: { minimum: 4, maximum: 200 }
+	validates :userid, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_195933) do
+ActiveRecord::Schema.define(version: 2020_07_09_113443) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
